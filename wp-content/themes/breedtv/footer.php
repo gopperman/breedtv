6a39f5ef1a765157633ae4111eee9cf1849a7948
Drew <drew.r.wilson@gmail.com>
2014-08-07 23:35:02 -0700

    add shortcut key for nexting a video

--- wp-content/themes/breedtv/footer.php
+++ wp-content/themes/breedtv/footer.php
@@ -1,5 +1,13 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
 <?php wp_footer(); ?>
+  <script charset="utf-8">
+    $( "body" ).keypress(function( event ) {
+      if (event.charCode == 107) {
+        $('#next').click();
+      }
+
+    });
+  </script>
 </body>
 </html>
